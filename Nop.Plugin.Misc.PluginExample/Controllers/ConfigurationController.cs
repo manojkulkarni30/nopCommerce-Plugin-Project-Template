ef1799f2b8b22b22271dd8c95b4b4af0917a9654
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2016-07-02 13:18:33 +0530

    Initial Checkin

---
+++ Nop.Plugin.Misc.PluginExample/Controllers/ConfigurationController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Misc.PluginExample.Controllers
+{
+    public class ConfigurationController // : BasePluginController
+    {
+
+    }
+}
---
+++ Nop.Plugin.Misc.PluginExample/PluginSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+//using Nop.Core.Plugins;
+//using Nop.Services.Common;
+
+namespace Nop.Plugin.Misc.PluginExample
+{
+    public class PluginSettings // : BasePlugin, IMiscPlugin
+    {
+
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "Configuration";
+            routeValues = new RouteValueDictionary { { "Namespaces", "$projectname$.Controllers" }, { "area", null } };
+        }
+    }
+}
